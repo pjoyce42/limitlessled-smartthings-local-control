cd81bd1f671450193ad973d3086b99ca5568ebb2
pjoyce42 <patrickwjoyce1@gmail.com>
2016-06-06 20:33:27 -0500

    housekeeping

--- RGBW_lights.groovy
+++ RGBW_lights.groovy
@@ -1,49 +1,51 @@
 /**
- *  HTTP Requests
+ *  RGBW Lights
  *
- *  Copyright 2016 Patrick Joyce (Credit goes to: Brian Freund (@brianfreund), Patrick Stuart (@pstuart) and Jason E (jasone) for their code examples I copied plus of course the SmartThings Documentation which helped immensely)
+ *  Patrick Joyce (Credit goes to: Brian Freund (@brianfreund) whose HTTP request app I used as the basis for this app.
  *
-*This is free and unencumbered software released into the public domain.
-*
-* Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org/>
+ * This is free and unencumbered software released into the public domain.
  *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ *  of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org/>
+ * 
  * Changes made by Patrick Joyce:
- 1. Replace all instances of "Android" with "Server"
- 2. Changed name, namespace, and author to prevent confusion
- 3. Updated description to match planned changes
- 4. Changed License from Apache License, version 2.0 to "the Unlicensed"
- 5. Change icon URLs 
+ * 1. Replace all instances of "Android" with "Server"
+ * 2. Changed name, namespace, and author to prevent confusion
+ * 3. Updated description to match planned changes
+ * 4. Changed License from Apache License, version 2.0 to "the Unlicensed"
+ * 5. Changed icon URLs to openly licensed icon
+ * 6. Added change log as required by license of the source app.
+ 
  */
 definition(
 name: "RGBW lights",
 namespace: "pjoyce42",
 author: "Patrick Joyce",
-description: "A SmartApp that when paired with  running Automagic and UDP Sender, can control your Milights naively through UDP commands.",
-category: "Convenience",
-iconUrl: "http://apk-dl.com/detail/image/com.lierda.wifi-w250.png",
-iconX2Url: "http://apk-dl.com/detail/image/com.lierda.wifi-w250.png",
-iconX3Url: "http://apk-dl.com/detail/image/com.lierda.wifi-w250.png")
+description: "A SmartApp that can control your Milights naively through python script running on your local network.",
+category: "Lights",
+iconUrl: "",
+iconX2Url: "",
+iconX3Url: "")
 
 preferences {
 section("Execute HTTP Request attached to switch") {
